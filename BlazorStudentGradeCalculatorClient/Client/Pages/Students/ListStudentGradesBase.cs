4d6ca253fbdd0eb116308cdf49c046625a55d197
Alfred A. Amos <alfredamos@live.com>
2021-03-26 05:20:06 +0100

    Add All Results page

---
+++ BlazorStudentGradeCalculatorClient/Client/Pages/Students/ListStudentGradesBase.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BlazorStudentGradeCalculatorClient.Client.Contracts;
+using BlazorStudentGradeCalculatorClient.Client.ViewModels;
+using BlazorStudentGradeCalculatorClient.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStudentGradeCalculatorClient.Client.Pages.Students
+{
+    public class ListStudentGradesBase : ComponentBase
+    {
+        [Inject]
+        public IStudentService StudentService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IMapper Mapper { get; set; }
+
+        public List<Student> StudentsDB { get; set; } = new List<Student>();
+
+        public List<StudentView> Students { get; set; } = new List<StudentView>();
+
+        public StudentView StudentTemp { get; set; } = new StudentView();
+
+        protected async override Task OnInitializedAsync()
+        {
+            StudentsDB = (await StudentService.GetAll()).ToList();
+
+            Mapper.Map(StudentsDB, Students);
+        }
+    }
+}
